f8e4c0c4c8ba69b41ba087dcdf1a7d9b0acc5c21
ubicapaukov <vladeskisenpai@gmail.com>
2023-04-27 17:42:18 +0300

    Version - 0.0.1_h: Bug fixes.

--- Assets/_Game/Scripts/Infrastructure/Services/Global/Save And Load/SaveAndLoadService.cs
+++ Assets/_Game/Scripts/Infrastructure/Services/Global/Save And Load/SaveAndLoadService.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace Core.Infrastructure.Services.Global
 {
-    #warning Need rework.
+#warning Need rework.
     public class SaveAndLoadService : ISaveAndLoadService
     {
         public SaveAndLoadService(IGameDataService gameDataService)
@@ -13,22 +13,27 @@ namespace Core.Infrastructure.Services.Global
             _gameDataService = gameDataService;
             _gameData = gameDataService.GetGameData();
 
+#if UNITY_EDITOR
             CheckGridHelper();
+#endif
             LoadGameData();
         }
-        
+
         private readonly IGameDataService _gameDataService;
         private readonly GameData _gameData;
         private readonly string _dataPath;
 
         public async void SaveGameData()
         {
+#if UNITY_EDITOR
             string gameDataJson = JsonUtility.ToJson(_gameData);
             await File.WriteAllTextAsync(_dataPath, gameDataJson);
+#endif
         }
 
         public void LoadGameData()
         {
+#if UNITY_EDITOR
             if (!File.Exists(_dataPath))
             {
                 SaveGameData();
@@ -42,6 +47,11 @@ namespace Core.Infrastructure.Services.Global
 
             GameData gameData = JsonUtility.FromJson<GameData>(data);
             _gameDataService.SetGameData(gameData);
+#elif UNITY_ANDROID || UNITY_IOS
+            TextAsset textAsset = Resources.Load<TextAsset>("GameConfig");
+            GameData gameData = JsonUtility.FromJson<GameData>(textAsset.text);
+            _gameDataService.SetGameData(gameData);
+#endif
         }
 
         private void CheckGridHelper()
@@ -57,7 +67,8 @@ namespace Core.Infrastructure.Services.Global
 
         private static string[] GetGridHelper()
         {
-            string[] gridHelper = {
+            string[] gridHelper =
+            {
                 " -> Don't edit! <- ",
                 "0 1 2 3 4 5 6 7 8 9",
                 "1 ☐ ☐ ☐ ☐ ☐ ☐ ☐ ☐ ☐",
